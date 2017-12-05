4c11178aa1f5c8d0319e52ff9476d53cfd3944b0
Jason Hsi <jhsi@alaska.edu>
2017-12-05 13:11:49 -0900

    added branching and datasize converters

---
+++ GNUToNASM/Converters/BranchConverter.cpp
@@ -0,0 +1,55 @@
+/*
+	BranchConverter.cpp
+	Authors: Frank Cline, Jason Hsi, Brandon Abbot
+	04 Dec 2017
+
+	cpp file for class for branch instructions
+	MADE BY JASON
+*/
+
+#include "stdafx.h"
+
+#include "BranchConverter.h"
+using std::shared_ptr;
+using std::string;
+
+// TODO: remove include
+#include <iostream>
+using std::endl;
+using std::cout;
+
+BranchConverter::BranchConverter(shared_ptr<GNULexer> & lexer, string instruction)
+{
+	_instruction = instruction;
+	_lexer = lexer;
+	_param0 = _lexer->GetNextLexeme();
+}
+
+// Convert
+// converts the ARM add instruction to x86
+string BranchConverter::Convert()
+{
+	cout << "0: " << _param0 << endl;
+	if (_instruction == "b") {
+		_instruction = "jmp";
+	}
+	else if (_instruction == "beq") {
+		_instruction = "je";
+	}
+	else if (_instruction == "bne") {
+		_instruction = "jne";
+	}
+	else if (_instruction == "blt") {
+		_instruction = "jl";
+	}
+	else if (_instruction == "bgt") {
+		_instruction = "jg";
+	}
+	else if (_instruction == "ble") {
+		_instruction = "jle";
+	}
+	else if (_instruction == "bge") {
+		_instruction = "jge";
+	}
+	return _instruction + " " + _param0 + "\n";
+}
---
+++ GNUToNASM/Converters/DatasizeConverter.cpp
@@ -0,0 +1,48 @@
+/*
+BranchConverter.cpp
+Authors: Frank Cline, Jason Hsi, Brandon Abbot
+04 Dec 2017
+
+cpp file for class for branch instructions
+MADE BY JASON
+*/
+
+#include "stdafx.h"
+#include "DatasizeConverter.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+DatasizeConverter::DatasizeConverter(std::shared_ptr<GNULexer>& lexer, std::string datasize)
+{
+	_datasize = datasize;
+	_lexer = lexer;
+	_param0 = _lexer->GetNextLexeme();
+}
+
+std::string DatasizeConverter::Convert()
+{
+	if (_datasize == ".data")
+	{
+		_datasize = "SECTION .DATA";
+		_param0 = "";
+	}
+	else if (_datasize == ".byte") 
+	{
+		_datasize = "db";
+	}
+	else if (_datasize == ".halfword") 
+	{
+		_datasize = "dw";
+	}
+	else if (_datasize == ".word")
+	{
+		_datasize = "dd";
+	}
+	else if (_datasize == ".doubleword")
+	{
+		_datasize = "dq";
+	}
+	return _datasize + " " +_param0 + "\n";
+}
