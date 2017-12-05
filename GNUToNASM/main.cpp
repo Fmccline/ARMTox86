15acdf57b54105a34ba37a19b6d03758a72f2239
Frank Cline <frankmcline@gmail.com>
2017-12-05 13:07:13 -0900

    Added test for arithmetic instructions

--- GNUToNASM/main.cpp
+++ GNUToNASM/main.cpp
@@ -39,8 +39,48 @@ void testMov()
 	cout << t.GetTestResults() << endl;
 }
 
+void testArithmetic()
+{
+	string testName, input, expectedOutput;
+
+	testName = "Arithmetic instructions";
+
+	input += "add r0,r1,r2\n";
+	input += "add r0,r0,#80\n";
+	input += "add r0,#80,#100\n";
+
+	input += "sub r0,r1,r2\n";
+	input += "sub r0,r0,#80\n";
+	input += "sub r0,#80,#100\n";
+
+	input += "mul r0,r1,r2\n";
+	input += "mul r0,r0,#80\n";
+	input += "mul r0,#80,#100\n";
+
+	expectedOutput += "mov rax,rsi\n";
+	expectedOutput += "add rax,rdx\n";
+	expectedOutput += "add rax,80\n";
+	expectedOutput += "mov rax,80\n";
+	expectedOutput += "add rax,100\n";
+
+	expectedOutput += "mov rax,rsi\n";
+	expectedOutput += "sub rax,rdx\n";
+	expectedOutput += "sub rax,80\n";
+	expectedOutput += "mov rax,80\n";
+	expectedOutput += "sub rax,100\n";
+
+	expectedOutput += "mov rax,rsi\n";
+	expectedOutput += "imul rax,rdx\n";
+	expectedOutput += "imul rax,80\n";
+	expectedOutput += "mov rax,80\n";
+	expectedOutput += "imul rax,100\n";
+
+	Test t(testName, input, expectedOutput);
+	cout << t.GetTestResults() << endl; 
+}
 
 int main()
 {
 	testMov();
+	testArithmetic();
 }
