017c7b17bb994c82343270b259584557988bbecb
Jason Hsi <jhsi@alaska.edu>
2017-12-06 02:45:12 -0900

    bitwise converters and relevant factory changes

---
+++ GNUToNASM/Converters/BitwiseConverter.cpp
@@ -0,0 +1,34 @@
+/*
+	BranchConverter.cpp
+	Authors: Frank Cline, Jason Hsi, Brandon Abbot
+	04 Dec 2017
+
+	cpp file for class for branch instructions
+	MADE BY JASON
+*/
+
+#include "BitwiseConverter.h"
+
+BitwiseConverter::BitwiseConverter(std::shared_ptr<GNULexer>& lexer, std::string instruction)
+{
+	_lexer = lexer;
+	_instruction = instruction;
+	_param0 = lexer->GetNextLexeme();
+}
+
+std::string BitwiseConverter::Convert()
+{
+	if (_instruction == "lsl")
+	{
+		_instruction = "shl";
+	}
+	else if (_instruction == "lsr")
+	{
+		_instruction = "shr";
+	}
+	else if (_instruction == "asr")
+	{
+		_instruction = "sar";
+	}
+	return _instruction + " " + _param0 + "\n";
+}
---
+++ GNUToNASM/Converters/BitwiseConverter.h
@@ -0,0 +1,30 @@
+/*
+	BitwiseConverter.h
+	Authors: Frank Cline, Jason Hsi, Brandon Abbot
+	04 Dec 2017
+
+	cpp file for class that converts branch instructions
+	MADE BY JASON
+*/
+
+#ifndef BITWISE_CONVERTER_H_INCLUDED
+#define BITWISE_CONVERTER_H_INCLUDED
+
+#include "Converter.h"
+#include "../Lexer/GNULexer.h"
+
+#include <memory>
+#include <string>
+
+// Currently only converts signed values
+class BitwiseConverter : public Converter
+{
+private:
+	std::shared_ptr<GNULexer> _lexer;
+	std::string _instruction, _param0;
+public:
+	BitwiseConverter(std::shared_ptr<GNULexer> & lexer, std::string instruction);
+	std::string Convert() override;
+};
+
+#endif // BITWISE_CONVERTER_H_INCLUDED
